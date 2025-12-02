5e32548cf381ca60bb522c3d306f186bd4117955
Matthew Eager <darthmajo@hotmail.com>
2025-12-02 14:56:23 +0100

    Skeleton for a new project
    Made the beginnings to a new game idea, just to start off with a simple
    2D project first.

---
+++ lair-construct/src/map.py
@@ -0,0 +1,47 @@
+# The prototype for a town
+
+class Map():
+    def __init__(self, x, y):
+        self.map = self.init_map(x, y)
+        self.sizeX = x
+        self.sizeY = y
+
+    def get_area(self):
+        return self.sizeX * self.sizeY
+    
+    def get_area_usable(self):
+        return (self.sizeX - 1) * (self.sizeY - 1)
+
+    def get_tile(self, x, y):
+        if not self.is_tile_in_bounds(x,y):
+            return -1
+
+        return self.map[y][x]
+    
+    def is_tile_in_bounds(self, x, y):
+        if x < 0 or x >= self.sizeX:
+            return False
+        if y < 0 or y >= self.sizeY:
+            return False
+        return True
+
+    def init_map(self, x, y):
+        mapY = []
+
+        for _ in range(y):
+            mapX = []
+            for _ in range(x):
+                mapX.append(0)
+            mapY.append(mapX)
+
+        return mapY
+    
+    def print_map(self):
+        for y in range(self.sizeY):
+            line = ""
+            for x in range(self.sizeX):
+                line += str(chr(self.map[y][x]))
+            print(line)
+
+    def set_tile(self, x, y, new_value):
+        self.map[y][x] = new_value
---
+++ lair-construct/src/room-generator.py
@@ -0,0 +1,5 @@
+# This will create the individual room in a map
+
+class RoomGenerator():
+    def __init__(self):
+        pass
